83004551c2d288e69ee69807a327d201327c3430
Wutao1530663 <1530663@tongji.edu.cn>
2017-02-27 15:48:56 +0800

    修正一个bug,bug导致每次存储数据进入数据库时会将之前的旧数据重复存储。

--- html_outputer.py
+++ html_outputer.py
@@ -2,17 +2,17 @@
 import pymysql
 class HtmlOutputer(object):
     def __init__(self):
-        self.book_ids = set()
-
-    def college(self, new_book_ids):
-        if new_book_ids is None:
+        self.old_book_ids = set()
+        self.new_book_ids = set()
+    def college(self, book_ids):
+        if book_ids is None:
             return
-        for new_book_id in new_book_ids:
-            if new_book_id not in self.book_ids:
-                self.book_ids.add(new_book_id)
+        for book_id in book_ids:
+            if book_id not in self.old_book_ids and book_id not in self.new_book_ids:
+                self.new_book_ids.add(book_id)
 
     def book_ids_size(self):
-        return len(self.book_ids)
+        return len(self.old_book_ids)
     def output_database(self):
         connect = pymysql.Connect(host='localhost',
                                   port=3306,
@@ -20,17 +20,16 @@ class HtmlOutputer(object):
                                   passwd='1234',
                                   db='spider',
                                   charset='utf8')
-
-
-
         try:
-            for book_id in self.book_ids:
+            for book_id in self.new_book_ids:
                 sql = "INSERT INTO book(id) VALUE (%s)"
                 with connect.cursor() as cursor:
                     cursor.execute(sql,(book_id))
                 connect.commit()
-            print ("%d条数据存储成功"% len(self.book_ids))
+                self.old_book_ids.add(book_id)
+            print ("%d条数据存储成功,已存数据%d条"% len(self.new_book_ids),len(self.old_book_ids))
         finally:
+            self.new_book_ids = set()
             connect.close()
 
     
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
-import url_manager,html_parser,image_downloader,html_downloader,html_outputer
-
+import url_manager, html_parser, image_downloader, html_downloader, html_outputer
+import threading
+import os
+import signal
 
 
 class bookSPider(object):
@@ -9,32 +11,40 @@ class bookSPider(object):
         self.downloader = html_downloader.HtmlDownloader()
         self.parser = html_parser.HtmlParser()
         self.outputer = html_outputer.HtmlOutputer()
-
-
-    def craw(self,root_url):
+        
+    def craw(self, root_url):
         self.urls.add_new_url(root_url)
+        count = 0
         while self.urls.has_new_url():
-             print("已经收集 %d 条书籍数据" % self.outputer.book_ids_size())
-             try :
+            count += 1
+            print("已经收集 %d 条书籍数据" % self.outputer.book_ids_size())
+            try:
                 new_url = self.urls.get_new_url()
-                print("正在抓取页面 %s"%(new_url))
+                print("正在抓取页面 %s" % (new_url))
                 html_content = self.downloader.download_html(new_url)
                 new_urls, new_book_ids = self.parser.parse(html_content)
                 self.urls.add_new_urls(new_urls)
-                #self.urls.add_new_book_urls(new_book_urls)
+                # self.urls.add_new_book_urls(new_book_urls)
                 self.outputer.college(new_book_ids)
-             except:
+                if count % 10 == 0:
+                    self.outputer.output_database()
+            except:
                 print("craw fail")
-        self.outputer.output_database()
+
+
 import urllib.request
 import requests
 
-if __name__ == '__main__':
 
+def sigint_hander():
+    spider.outputer.output_database()
+
+
+if __name__ == '__main__':
     root_url = "https://book.douban.com/"
     spider = bookSPider()
     spider.craw(root_url)
-  #a = 'bats\u00E0'
-  #print (str(a))
-  #
+    # a = 'bats\u00E0'
+    # print (str(a))
+    #
 
